2011b6420ec59dd237d69c81add78a602c332b9e
imarkovic1 <imarkovic@lioneight.com>
2019-12-06 15:24:36 +0100

    WIP.

--- js/init.js
+++ js/init.js
@@ -3,6 +3,9 @@
 
     $('.sidenav').sidenav();
     $('.parallax').parallax();
+    $('.fixed-action-btn').floatingActionButton({
+      'direction': 'left'
+    });
 
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+  });
+})(jQuery);
